e685181a6faebce3c493f5e5b98a5aba95460fae
Matias <tute2286@gmail.com>
2021-07-09 16:53:12 -0400

    + Multibrowser support

--- context/driver.py
+++ context/driver.py
@@ -6,21 +6,16 @@ class Driver(object):
     class SeleniumDriverNotFound(Exception):
         pass
 
-    def __init__(self, tag_browser=None):
-
-        if tag_browser:
-            if tag_browser == "chrome":
-                self.driver = webdriver.Chrome()
-                self.driver.maximize_window()
+    def __init__(self, tag_browser=settings.default_browser):
+        if tag_browser == "chrome":
+            self.driver = webdriver.Chrome()
+            self.driver.maximize_window()
+        elif tag_browser == "firefox":
+            self.driver = webdriver.Firefox()
+            self.driver.maximize_window()
         else:
-            if settings.default_browser == "chrome":
-                self.driver = webdriver.Chrome()
-                self.driver.maximize_window()
-            elif settings.default_browser == "firefox":
-                self.driver = webdriver.Firefox()
-            else:
-                raise SeleniumDriverNotFound(
-                    f"{settings.browser} not currently supported")
+            raise SeleniumDriverNotFound(
+                f"{settings.browser} not currently supported")
 
     def get_driver(self):
         return self.driver
@@ -34,5 +29,3 @@ class Driver(object):
     def navigate(self, url):
         self.driver.get(url)
 
-
-#driver = Driver.get_instance()
